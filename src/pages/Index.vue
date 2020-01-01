4d186354bce4b4e3656dcb4d461ec9caa91d5e56
Robert Adamczewski <robert@adamczewski.me>
2020-01-01 14:33:53 +0100

    feat: add new empty pagesand default layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,144 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
-  </q-page>
+  <div class="q-pa-md">
+    <q-table :data="graves" :columns="columns" :pagination.sync="pagination" row-key="name">
+      <q-tr slot="body" slot-scope="props" class="text-center" :props="props">
+        <q-td key="desc" :props="props">
+          <span class="text-italic">{{ props.row.parcela.nrGrobu }}</span>
+          <q-tooltip>I'd like to eat "{{ props.row.name }}"</q-tooltip>
+        </q-td>
+        <q-td key="opcje" :props="props">
+          <div class="row items-center justify-between no-wrap">
+            <q-btn
+              size="sm"
+              round
+              dense
+              color="blue"
+              icon="visibility"
+              class="q-mr-xs"
+              @click="props.row.calories--"
+            >
+              <q-tooltip
+                anchor="top middle"
+                self="center middle"
+                transition-show="scale"
+                transition-hide="scale"
+                content-class="bg-blue-4"
+              >Pokaz</q-tooltip>
+            </q-btn>
+            <q-btn
+              size="sm"
+              round
+              dense
+              color="green"
+              icon="edit"
+              class="q-mr-sm"
+              @click="props.row.calories++"
+            >
+              <q-tooltip
+                anchor="top middle"
+                self="center middle"
+                transition-show="scale"
+                transition-hide="scale"
+                content-class="bg-green-4"
+              >Edytuj</q-tooltip>
+            </q-btn>
+            <q-btn
+              size="sm"
+              round
+              dense
+              color="red"
+              icon="remove"
+              class="q-mr-xs"
+              @click="props.row.calories--"
+            >
+              <q-tooltip
+                anchor="top middle"
+                self="center middle"
+                transition-show="scale"
+                transition-hide="scale"
+                content-class="bg-red-4"
+              >Usuń</q-tooltip>
+            </q-btn>
+          </div>
+        </q-td>
+        <q-td key="numerGrobu" :props="props">{{ props.row.parcela.nrGrobu }}</q-td>
+        <q-td key="rodzajGrobu" :props="props">{{ props.row.parcela.rodzaj }}</q-td>
+        <q-td key="statusGrobu" :props="props">
+          <span v-if="props.row.parcela.status !== 'Nie opłacony'">{{ props.row.parcela.status }}</span>
+          <q-chip v-else small square color="red-2">{{ props.row.parcela.status }}</q-chip>
+        </q-td>
+        <q-td key="dataZaplatyGrobu" :props="props">{{ props.row.parcela.dtZaplaty }}</q-td>
+      </q-tr>
+    </q-table>
+  </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
 
 export default {
-  // name: "PageIndex",
+  name: "PageIndex",
+  data() {
+    return {
+      pagination: {
+        sortBy: "numerGrobu",
+        descending: false,
+        page: 1,
+        rowsPerPage: 10
+      },
+      columns: [
+        {
+          name: "opcje",
+          label: "",
+          align: "center"
+        },
+        {
+          name: "numerGrobu",
+          label: "Nr. parceli",
+          align: "center",
+          field: graves => `${graves.parcela.parcela}${graves.parcela.rzad}${graves.parcela.grob}`,
+          filter: true,
+          sortable: true,
+          sort: (valA, valB) => {
+            // slice(2) removes 'cm' from cemetery name
+            let a = valA.slice(2),
+              b = valB.slice(2);
+
+            return isNaN(a - b) ? (a === b ? 0 : a < b ? -1 : 1) : a - b;
+          }
+        },
+        {
+          name: "rodzajGrobu",
+          label: "Rodzaj",
+          align: "center",
+          field: graves => graves.parcela.rodzaj,
+          sortable: true
+        },
+        {
+          name: "statusGrobu",
+          label: "Status",
+          align: "center",
+          field: graves => graves.parcela.status,
+          sortable: true
+        },
+        {
+          name: "dataZaplatyGrobu",
+          label: "Dt. zapłaty",
+          align: "center",
+          field: graves => graves.parcela.dtZaplaty,
+          sortable: true
+        }
+      ]
+    };
+  },
   computed: {
-    ...mapState("cm", ["graves"]),
-    ...mapState("cm", ["cemeteries"]),
-    ...mapState("cm", ["users"]),
-    ...mapState("cm", ["takers"])
+    ...mapState("cm", ["graves"])
   },
   created() {
     this["FETCH_GRAVES"]();
-    this["FETCH_CEMETERIES"]();
-    this["FETCH_USERS"]();
-    this["FETCH_TAKERS"]();
   },
   methods: {
-    ...mapActions("cm", ["FETCH_GRAVES"]),
-    ...mapActions("cm", ["FETCH_CEMETERIES"]),
-    ...mapActions("cm", ["FETCH_USERS"]),
-    ...mapActions("cm", ["FETCH_TAKERS"])
+    ...mapActions("cm", ["FETCH_GRAVES"])
   }
 };
 </script>
