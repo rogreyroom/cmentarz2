19fd4e65a7b58850390c23e67ce7b4397dbb4325
Robert Adamczewski <robert@adamczewski.me>
2020-03-02 19:02:15 +0100

    feat: change double quotes to single quotes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,71 +1,71 @@
 const routes = [
-  {
-    path: "/",
-    component: () => import("layouts/Default.vue"),
-    children: [
-      {
-        path: "",
-        name: "home",
-        component: () => import("pages/Index.vue")
-      },
-      {
-        path: "/cemetery-map",
-        name: "cemetery-map",
-        component: () => import("pages/CemeteryMap.vue")
-      },
-      {
-        path: "/cemetery-add",
-        name: "cemetery-add",
-        component: () => import("pages/AddCemetery.vue")
-      },
-      {
-        path: "/grave-add",
-        name: "grave-add",
-        component: () => import("pages/AddGrave.vue")
-      },
-      {
-        path: "/full-list",
-        name: "graves-full-list",
-        component: () => import("pages/ListGraves.vue")
-      },
-      {
-        path: "/unpaid-list",
-        name: "unpaid-list",
-        component: () => import("pages/UnpaidGraves.vue")
-      },
-      {
-        path: "/dead-list",
-        name: "dead-list",
-        component: () => import("pages/DeadByYear.vue")
-      },
-      {
-        path: "/grave-show/",
-        name: "grave-show",
-        component: () => import("pages/ShowGrave.vue"),
-        props: true
-      },
-      {
-        path: "/grave-edit/",
-        name: "grave-edit",
-        component: () => import("pages/EditGrave.vue"),
-        props: true
-      },
-      {
-        path: "/grave-delete/",
-        name: "grave-delete",
-        component: () => import("pages/ShowGrave.vue"),
-        props: true
-      }
-    ]
-  }
+	{
+		path: '/',
+		component: () => import('layouts/Default.vue'),
+		children: [
+			{
+				path: '',
+				name: 'home',
+				component: () => import('pages/Index.vue')
+			},
+			{
+				path: '/cemetery-map',
+				name: 'cemetery-map',
+				component: () => import('pages/CemeteryMap.vue')
+			},
+			{
+				path: '/cemetery-add',
+				name: 'cemetery-add',
+				component: () => import('pages/AddCemetery.vue')
+			},
+			{
+				path: '/grave-add',
+				name: 'grave-add',
+				component: () => import('pages/AddGrave.vue')
+			},
+			{
+				path: '/full-list',
+				name: 'graves-full-list',
+				component: () => import('pages/ListGraves.vue')
+			},
+			{
+				path: '/unpaid-list',
+				name: 'unpaid-list',
+				component: () => import('pages/UnpaidGraves.vue')
+			},
+			{
+				path: '/dead-list',
+				name: 'dead-list',
+				component: () => import('pages/DeadByYear.vue')
+			},
+			{
+				path: '/grave-show/:id',
+				name: 'grave-show',
+				component: () => import('pages/ShowGrave.vue'),
+				props: true
+			},
+			{
+				path: '/grave-edit/',
+				name: 'grave-edit',
+				component: () => import('pages/EditGrave.vue'),
+				props: true
+			},
+			{
+				path: '/grave-delete/',
+				name: 'grave-delete',
+				component: () => import('pages/ShowGrave.vue'),
+				props: true
+			}
+		]
+	}
 ];
 
 // Always leave this as last one
-if (process.env.MODE !== "ssr") {
-  routes.push({
-    path: "*",
-    component: () => import("pages/Error404.vue")
-  });
+if (process.env.MODE !== 'ssr') {
+	routes.push({
+		path: '*',
+		component: () => import('pages/Error404.vue')
+	});
 }
 
 export default routes;
