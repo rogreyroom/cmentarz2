1ae00102b2423628d7c989110491185beb1dae92
Robert Adamczewski <robert@adamczewski.me>
2020-03-04 21:38:36 +0100

    feat: update to display edit form

--- src/pages/EditGrave.vue
+++ src/pages/EditGrave.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="q-pa-md">
-    edit grave - {{ id }}
+    <edit-grave-form :id="id" />
   </div>
 </template>
 
 <script>
+
+import EditGrave from '../components/Forms/EditGrave'
+
 export default {
+  components: {
+    'edit-grave-form': EditGrave
+  },
   data () {
     return {
       id: this.$route.params.id,
