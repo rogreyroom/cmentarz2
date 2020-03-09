6059e80caafd83f07ded04bf9c936f388d8db41d
Robert Adamczewski <robert@adamczewski.me>
2020-03-09 19:36:08 +0100

    feat: add template to User and Grave view components

---
+++ src/components/Views/Grave.vue
@@ -0,0 +1,79 @@
+<template>
+  <section>
+    <div class="row q-pa-sm q-gutter-sm">
+      <h5>
+        Dane grobu:
+        <strong class="q-ml-sm">{{ graveId }}</strong>
+      </h5>
+      <q-btn
+        :to="{ name: 'grave-edit', params: { id: graveId }}"
+        flat
+        icon="edit"
+        class="q-ml-md"
+        text-color="light-blue-13"
+      />
+      <q-btn
+        :to="{ name: 'grave-edit', params: { id: graveId }}"
+        flat
+        icon="map"
+        class="q-ml-md"
+        text-color="light-blue-13"
+      />
+    </div>
+    <hr>
+    <div class="row q-pa-sm q-gutter-sm">
+      <div class="col">
+        <q-img
+          src="https://placeimg.com/500/300/nature"
+          style="max-width: 400px; height: 200px;"
+        />
+        <!-- {{ graveData.ext }} -->
+      </div>
+      <div class="col q-ml-lg justify-start">
+        <div class="row">
+          <p><strong>Parcela:</strong> {{ grave.parcela }}</p>
+        </div>
+        <div class="row">
+          <p><strong>Rząd:</strong> {{ grave.rzad }}</p>
+        </div>
+        <div class="row">
+          <p><strong>Numer grobu:</strong> {{ grave.grob }}</p>
+        </div>
+        <div class="row">
+          <p><strong>Rodzaj grobu:</strong> {{ grave.rodzaj }}</p>
+        </div>
+        <div class="row">
+          <p><strong>Status grobu:</strong> {{ grave.status }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="row q-pa-sm q-gutter-sm">
+      <div class="col">
+        <p><strong>Okres opłaty:</strong> {{ grave.okres }}</p>
+      </div>
+      <div class="col">
+        <p><strong>Data opłaty:</strong> {{ grave.dtOplaty }}</p>
+      </div>
+    </div>
+    <div class="row q-pa-sm q-gutter-sm">
+      <p><strong>Uwagi:</strong> {{ grave.uwagi }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    graveId: {
+      type: String,
+      default: ''
+    },
+    grave: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  },
+};
+</script>
---
+++ src/components/Views/User.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <div class="row q-pa-sm q-gutter-sm">
+      <h5>
+        Dane osoby zmarłej
+      </h5>
+      <q-btn
+        :to="{ name: 'grave-add-user', params: { id: graveId } }"
+        flat
+        icon="add"
+        class="q-ml-md"
+        text-color="light-blue-13"
+      />
+    </div>
+    <div
+      v-for="{_id, user} in users"
+      :key="_id"
+      class="row q-pa-sm q-gutter-sm"
+    >
+      <single-user
+        :id="_id"
+        :user="user"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import SingleUser from '../components/Views/SingleUser'
+
+export default {
+  components: {
+    'single-user': SingleUser
+  },
+  props: {
+    graveId: {
+      type: String,
+      default: ''
+    },
+    users: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  }
+};
+</script>
