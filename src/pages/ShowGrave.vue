991307abadb22725acb00ab79063cc93ae60c82c
Robert Adamczewski <robert@adamczewski.me>
2020-03-09 19:37:51 +0100

    feat: move templates to User and Grave view components and import them to the showGrave page

--- src/pages/ShowGrave.vue
+++ src/pages/ShowGrave.vue
@@ -1,64 +1,11 @@
 <template>
   <div class="q-pa-md">
     <q-page padding>
-      <div class="row q-pa-sm q-gutter-sm">
-        <h5>
-          Dane grobu:
-          <strong class="q-ml-sm">{{ id }}</strong>
-        </h5>
-        <q-btn
-          :to="{ name: 'grave-edit', params: {id: id} }"
-          flat
-          icon="edit"
-          class="q-ml-md"
-          text-color="light-blue-13"
-        />
-        <q-btn
-          :to="{ name: 'grave-edit', params: {id: id} }"
-          flat
-          icon="map"
-          class="q-ml-md"
-          text-color="light-blue-13"
-        />
-      </div>
-      <hr>
-      <div class="row q-pa-sm q-gutter-sm">
-        <div class="col">
-          <q-img
-            src="https://placeimg.com/500/300/nature"
-            style="max-width: 400px; height: 200px;"
-          />
-          <!-- {{ graveData.ext }} -->
-        </div>
-        <div class="col q-ml-lg justify-start">
-          <div class="row">
-            <p><strong>Parcela:</strong> {{ graveData.parcela }}</p>
-          </div>
-          <div class="row">
-            <p><strong>Rząd:</strong> {{ graveData.rzad }}</p>
-          </div>
-          <div class="row">
-            <p><strong>Numer grobu:</strong> {{ graveData.grob }}</p>
-          </div>
-          <div class="row">
-            <p><strong>Rodzaj grobu:</strong> {{ graveData.rodzaj }}</p>
-          </div>
-          <div class="row">
-            <p><strong>Status grobu:</strong> {{ graveData.status }}</p>
-          </div>
-        </div>
-      </div>
-      <div class="row q-pa-sm q-gutter-sm">
-        <div class="col">
-          <p><strong>Okres opłaty:</strong> {{ graveData.okres }}</p>
-        </div>
-        <div class="col">
-          <p><strong>Data opłaty:</strong> {{ graveData.dtOplaty }}</p>
-        </div>
-      </div>
-      <div class="row q-pa-sm q-gutter-sm">
-        <p><strong>Uwagi:</strong> {{ graveData.uwagi }}</p>
-      </div>
+      <!-- Grave view component -->
+      <grave
+        graveId="id"
+        :grave="graveData"
+      />
       <hr>
       <!-- Grave taker -->
       <div class="row q-pa-sm q-gutter-sm">
@@ -87,29 +34,10 @@
       </div>
       <hr>
       <!-- Grave users -->
-      <div class="row q-pa-sm q-gutter-sm">
-        <h5>
-          Dane osoby zmarłej
-        </h5>
-        <q-btn
-          :to="{ name: 'grave-add-user', params: {id: id} }"
-          flat
-          icon="add"
-          class="q-ml-md"
-          text-color="light-blue-13"
-        />
-      </div>
-      <div
-        v-for="{_id, user} in usersData"
-        :key="_id"
-        class="row q-pa-sm q-gutter-sm"
-      >
-        <!-- {{user}} -->
-        <grave-user
-          :id="_id"
-          :user="user"
-        />
-      </div>
+      <user
+        graveId="id"
+        :users="usersData"
+      />
       <hr>
     </q-page>
   </div>
@@ -117,11 +45,13 @@
 
 <script>
 import { mapGetters } from "vuex";
-import GraveUser from '../components/GraveUser'
+import Grave from '../components/Views/Grave'
+import User from '../components/Views/User'
 
 export default {
   components: {
-    'grave-user': GraveUser
+    'grave': Grave,
+    'user': User
   },
   data () {
     return {
