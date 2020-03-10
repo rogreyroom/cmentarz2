c9e447daa8eef9c17335823e77f0a32ed6793283
Robert Adamczewski <robert@adamczewski.me>
2020-03-10 19:16:55 +0100

    feat: add global hr style to the app

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,15 @@ export default {
   name: "App"
 };
 </script>
+
+<style>
+hr {
+  display: block;
+  border: 0;
+  height: 0;
+  width: 96%;
+  margin: 1rem auto;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+}
+</style>
