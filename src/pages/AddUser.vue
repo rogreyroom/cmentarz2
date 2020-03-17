c7e449ca1ed3ace5010a37f3b4bb5b2e6f74e288
Robert Adamczewski <robert@adamczewski.me>
2020-03-17 15:38:45 +0100

    feat: add AddUser page

---
+++ src/pages/AddUser.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="q-pa-md">
+    <q-page padding>
+      <header class="row q-pa-sm q-gutter-sm">
+        <h5>
+          Dodaj zmarłego do grobu:
+          <strong class="q-ml-sm">{{ id }}</strong>
+        </h5>
+        <hr>
+      </header>
+      <section>
+        <user-form
+          :id="id"
+          :user="user"
+          flag="edit"
+        />
+      </section>
+    </q-page>
+  </div>
+</template>
+
+<script>
+import User from '../components/Forms/User'
+
+export default {
+  components: {
+    'user-form': User
+  },
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    user: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
