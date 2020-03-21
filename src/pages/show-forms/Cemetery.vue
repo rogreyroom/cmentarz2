a77d1bbb389c8c09ebc9e345767e6fc9fb240904
Robert Adamczewski <robert@adamczewski.me>
2020-03-21 13:40:15 +0100

    feat: add testing props to the component

--- src/pages/show-forms/Cemetery.vue
+++ src/pages/show-forms/Cemetery.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="q-pa-md">
     add cemetery
+    (show-form)
+    <hr>
+    <div>
+      id: {{ id }}
+    </div>
+    <hr>
+    <div>
+      dane:
+      {{ cemetery }}
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-
-  };
+export default {
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    cemetery: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  }
+};
 </script>
 
 <style></style>
