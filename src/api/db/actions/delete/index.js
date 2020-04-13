0aabf9bb998559d19d6a3c57ee8e41b267458f84
Robert Adamczewski <robert@adamczewski.me>
2020-04-13 19:43:59 +0200

    feat: add functions to remove single grave data from the database

--- src/api/db/actions/delete/index.js
+++ src/api/db/actions/delete/index.js
@@ -5,3 +5,7 @@ import { db } from '../../index';
 export const REMOVE_CEMETERY_DATA = async id => {
 	return await db.asyncRemove({ _id: id });
 };
+
+export const REMOVE_GRAVE_DATA = async grave => {
+	return await db.asyncRemove({ 'parcela.nrGrobu': grave });
+};
