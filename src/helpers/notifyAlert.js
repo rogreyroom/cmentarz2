5086def59c99e5c5707300e9de9a66bb0a47da32
Robert Adamczewski <robert@adamczewski.me>
2020-04-13 10:56:07 +0200

    feat: add an argument to set timeout manually

--- src/helpers/notifyAlert.js
+++ src/helpers/notifyAlert.js
@@ -1,9 +1,9 @@
-export const notifyAlert = function(msg, msgType) {
+export const notifyAlert = function(msg, msgType, time) {
 	return this.$q.notify({
 		message: msg,
 		color: msgType === 'error' ? 'negative' : 'positive',
 		position: 'center',
 		icon: 'announcement',
-		timeout: 2500
+		timeout: time || 2500
 	});
 };
