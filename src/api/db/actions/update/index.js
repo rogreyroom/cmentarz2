981afd74ec7e1047a99529405cc84b5382ef977e
Robert Adamczewski <robert@adamczewski.me>
2020-04-19 12:04:37 +0200

    feat: add database functions to update grave and taker data

--- src/api/db/actions/update/index.js
+++ src/api/db/actions/update/index.js
@@ -9,3 +9,38 @@ export const UPDATE_CEMETERY_DATA = async (id, value) => {
 		{ $set: { 'thecm.cmFullName': cmFullName, 'thecm.wejscie': wejscie, 'thecm.rzad': rzad, 'thecm.grob': grob } }
 	);
 };
+
+export const UPDATE_GRAVE_DATA = async (id, value) => {
+	const { rodzaj, status, okres, dtOplaty, dtZaplaty, ext, uwagi } = value;
+	return await db.asyncUpdate(
+		{ _id: id },
+		{
+			$set: {
+				'parcela.rodzaj': rodzaj,
+				'parcela.status': status,
+				'parcela.okres': okres,
+				'parcela.dtOplaty': dtOplaty,
+				'parcela.dtZaplaty': dtZaplaty,
+				'parcela.ext': ext,
+				'parcela.uwagi': uwagi
+			}
+		}
+	);
+};
+
+export const UPDATE_TAKER_DATA = async (id, value) => {
+	const { imie, nazwisko, adres, email, tel, uwagi } = value;
+	return await db.asyncUpdate(
+		{ _id: id },
+		{
+			$set: {
+				'taker.imie': imie,
+				'taker.nazwisko': nazwisko,
+				'taker.adres': adres,
+				'taker.email': email,
+				'taker.tel': tel,
+				'taker.uwagi': uwagi
+			}
+		}
+	);
+};
