705288b4b574c028dac254554748f54504ee3c2f
Robert Adamczewski <robert@adamczewski.me>
2020-05-03 11:37:42 +0200

    fix: move global hr style from app.vue to app.scss

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,3 @@ export default {
 };
 </script>
 
-<style>
-hr {
-  display: block;
-  border: 0;
-  height: 0;
-  width: 96%;
-  margin: 1rem auto;
-  border-top: 1px solid rgba(0, 0, 0, 0.1);
-  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
-}
-</style>
