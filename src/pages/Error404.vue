7aab256bfc65c52c7e98b6427b76ae08ca03e205
Robert Adamczewski <robert@adamczewski.me>
2020-05-12 20:29:00 +0200

    feat: add a new icon to 404 and change the descriptions

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -2,19 +2,32 @@
   <div class="fixed-center text-center">
     <p>
       <img
-        src="~assets/sad.svg"
+        src="~assets/cm.svg"
         style="width:30vw;max-width:150px;"
       >
     </p>
     <p class="text-faded">
-      Sorry, nothing here...<strong>(404)</strong>
+      Przykro mi, coś poszło nie tak...<strong>(404)</strong>
     </p>
     <q-btn
-      color="secondary"
-      style="width:200px;"
-      to="/"
-      label="Go back"
-    />
+      v-go-back.single=" '/' "
+      size="14px"
+      round
+      dense
+      color="indigo-9"
+      icon="arrow_back"
+      class="q-mr-sm"
+    >
+      <q-tooltip
+        anchor="top middle"
+        self="center middle"
+        transition-show="scale"
+        transition-hide="scale"
+        content-class="bg-blue-4"
+      >
+        Wróć
+      </q-tooltip>
+    </q-btn>
   </div>
 </template>
 
